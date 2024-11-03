9483929d20c909dd6c22be27a10f0c431e1e9c39
Ayushman Mishra <ayushman1729@gmail.com>
2024-11-03 13:43:12 +0530

    modified code editor

--- code_editor/urls.py
+++ code_editor/urls.py
@@ -3,4 +3,5 @@ from .views import RunCodeView
 
 urlpatterns = [
     path('code/', RunCodeView.as_view(), name='run-code'),
+    path('code/<int:pk>/', RunCodeView.as_view(), name='code-detail'),
 ]
